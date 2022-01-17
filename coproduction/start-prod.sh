2ff4bb3c003d264058fa37280bed4f571c46237e
pablodem <pablodecm@gmail.com>
2022-01-17 14:25:06 +0100

    Fix gunicorn_conf.py path (use absolute for now)

--- coproduction/start-prod.sh
+++ coproduction/start-prod.sh
@@ -8,6 +8,7 @@ LOG_LEVEL=${LOG_LEVEL:-info}
 # Let the DB start
 python /app/app/pre_start.py
 
+<<<<<<< HEAD
 # Run migrations
 alembic revision --autogenerate -m "Added initial table"
 alembic upgrade head
@@ -15,3 +16,6 @@ alembic upgrade head
 echo MIGRATIONS DONE
 
 exec gunicorn -k "uvicorn.workers.UvicornWorker" -c "gunicorn_conf.py" "app.main:app"
+=======
+exec gunicorn -k "uvicorn.workers.UvicornWorker" -c "/app/gunicorn_conf.py" "app.main:app"
+>>>>>>> Fix gunicorn_conf.py path (use absolute for now)
