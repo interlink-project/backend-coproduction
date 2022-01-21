a0a0e6f25ded457ad651a654c4d8e55aa7788f1c
Julen Badiola <33113693+julenbadiola@users.noreply.github.com>
2022-01-21 13:34:19 +0100

    Prestart wait for catalogue and acl

--- coproduction/app/pre_start.py
+++ coproduction/app/pre_start.py
@@ -1,7 +1,7 @@
 import logging
-
+import requests
 from tenacity import after_log, before_log, retry, stop_after_attempt, wait_fixed
-
+from app.config import settings
 from app.general.db.session import SessionLocal
 
 logging.basicConfig(level=logging.INFO)
@@ -27,9 +27,37 @@ def wait_for_database() -> None:
         raise e
 
 
+@retry(
+    stop=stop_after_attempt(max_tries),
+    wait=wait_fixed(wait_seconds),
+    before=before_log(logger, logging.INFO),
+    after=after_log(logger, logging.WARN),
+)
+def wait_for_ACL() -> None:
+    try:
+        requests.get(f"http://{settings.ACL_SERVICE_NAME}/healthcheck").json()
+    except Exception as e:
+        logger.error(e)
+        raise e
+
+@retry(
+    stop=stop_after_attempt(max_tries),
+    wait=wait_fixed(wait_seconds),
+    before=before_log(logger, logging.INFO),
+    after=after_log(logger, logging.WARN),
+)
+def wait_for_catalogue() -> None:
+    try:
+        requests.get(f"http://{settings.CATALOGUE_SERVICE_NAME}/healthcheck").json()
+    except Exception as e:
+        logger.error(e)
+        raise e
+
 def main() -> None:
     logger.info("Initializing service")
     wait_for_database()
+    wait_for_ACL()
+    wait_for_catalogue()
     logger.info("Services finished initializing")
 
 
