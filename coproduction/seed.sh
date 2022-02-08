fb398204972d4fe2ea319811b9e964ccd67f7a9f
Julen Badiola <33113693+julenbadiola@users.noreply.github.com>
2022-02-08 12:38:47 +0100

    Seed corrected

--- coproduction/seed.sh
+++ coproduction/seed.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
-# rm -rf /app/interlinkers-data || true
-# git clone https://github.com/interlink-project/interlinkers-data /app/interlinkers-data
+rm -rf /app/interlinkers-data || true
+git clone https://github.com/interlink-project/interlinkers-data /app/interlinkers-data
 python /app/app/initial_data.py
-# rm -rf /app/interlinkers-data || true
+rm -rf /app/interlinkers-data || true
