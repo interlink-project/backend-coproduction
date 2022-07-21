da381fb63fc1a0f49dc917014354c80407ebd2c5
Julen Badiola <badiolajulen@gmail.com>
2022-07-21 17:19:12 +0200

    Function names

--- coproduction/app/signals.py
+++ coproduction/app/signals.py
@@ -8,19 +8,19 @@ from app.config import settings
 @event.listens_for(Permission, "after_insert")
 @event.listens_for(Permission, "after_update")
 @event.listens_for(Permission, "after_delete")
-def after_insert(mapper, connection, target: Permission):
+def after_permission_insert_update_or_delete(mapper, connection, target: Permission):
     sync_asset_users.delay([target.treeitem_id])
 
 
 @event.listens_for(InternalAsset, "after_insert")
 @event.listens_for(InternalAsset, "after_update")
-def after_insert(mapper, connection, target: InternalAsset):
+def after_asset_insert_or_update(mapper, connection, target: InternalAsset):
     print("Asset created", target)
     sync_asset_users.delay([target.task_id])
 
 
 @event.listens_for(InternalAsset, "after_delete")
-def after_insert(mapper, connection, target: InternalAsset):
+def after_asset_delete(mapper, connection, target: InternalAsset):
     print("Asset deleted", target)
     URL = target.internal_link
     requests.delete(URL, headers={
