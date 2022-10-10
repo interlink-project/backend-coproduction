98526b4bdf41ee7984f6f24b95ba09340ee9ac2a
danielSilva21 <d.silva@deusto.es>
2022-10-10 11:55:55 +0200

    Changing to NullPool

--- coproduction/app/general/db/session.py
+++ coproduction/app/general/db/session.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
+from sqlalchemy.pool import NullPool
 from app.config import settings
 
-engine = create_engine(settings.SQLALCHEMY_DATABASE_URI, pool_pre_ping=True)
+engine = create_engine(settings.SQLALCHEMY_DATABASE_URI, poolclass=NullPool )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
