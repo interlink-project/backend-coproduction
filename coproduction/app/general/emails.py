02f7022b5b717034c2e76c55ca9bdc9e2f8f607e
Rubén Sánchez <ruben.sanchez@deusto.es>
2023-06-07 15:22:50 +0200

    Mail testing

--- coproduction/app/general/emails.py
+++ coproduction/app/general/emails.py
@@ -54,6 +54,7 @@ def send_email(
     message = emails.Message(
         subject=subject,
         html=template,
+        text=subject,
         mail_from=(settings.EMAILS_FROM_NAME, settings.EMAILS_FROM_EMAIL),
     )
 
