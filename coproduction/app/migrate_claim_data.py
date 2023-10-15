384f5ab92d81e9f9bf193bf6eb24f6fea1ec60ca
danielSilva21 <danyche2014@gmail.com>
2023-10-15 16:23:51 +0200

    Improving script to migrate.

--- coproduction/app/migrate_claim_data.py
+++ coproduction/app/migrate_claim_data.py
@@ -2,53 +2,47 @@ import psycopg2
 import os
 import json
 from datetime import datetime, timezone
-import uuid
-                    
-#Get the environment variables:
+
+# Get the environment variables:
 username = os.getenv("POSTGRES_USER", "postgres")
 password = os.getenv("POSTGRES_PASSWORD", "changethis")
 postgres_server = os.getenv("POSTGRES_SERVER", "localhost")
 postgres_db = os.getenv("POSTGRES_DB", "coproduction")
 
-def migrateDataClaims(connection=None):
+def create_connection():
+    try:
+        connection = psycopg2.connect(
+            user=username,
+            password=password,
+            host=postgres_server,
+            port="5432",
+            database=postgres_db
+        )
+        print("Connected to the database.")
+        return connection
+    except psycopg2.Error as e:
+        print(f"Error connecting to the database: {e}")
+        return None
 
+def migrate_data_claims(connection):
     total_inserted_rows = 0
-
-
-    #Establish the first time connection if dont exist
-    if not connection:
-        connection = psycopg2.connect(user=username,
-                                    password=password,
-                                    host=postgres_server,
-                                    port="5432",
-                                    database=postgres_db)
 
     cursor = connection.cursor()
 
-    #Fix coproduction tree references:
-
-
-    #Delete all previous claims of table:
+    # Delete all previous claims from the table:
     query_delete_claims = "DELETE FROM claim;"
     cursor.execute(query_delete_claims)
-    connection.commit()  # Commit the transaction
-    
+    print("Deleted previous claims.")
 
-    #Obtain all parents nodes with multiple dependants (inconsistences)
-
-    query_coproductionprocessnotification = "SELECT id,coproductionprocess_id, asset_id, user_id, parameters, created_at FROM coproductionprocessnotification WHERE claim_type='development';" 
-    
-    # Execute the query
+    # Obtain all parent nodes with multiple dependents (inconsistencies):
+    query_coproductionprocessnotification = """
+        SELECT id, coproductionprocess_id, asset_id, user_id, parameters, created_at
+        FROM coproductionprocessnotification
+        WHERE claim_type='development';
+    """
     cursor.execute(query_coproductionprocessnotification)
-
-    # Fetch all rows
     rows = cursor.fetchall()
 
-    # Print the number of rows
-    numeroRows = len(rows)
-    print(f'Number of rows: {numeroRows}')
-
-    # Iterate over rows
     for row in rows:
         id,coproductionprocess_id, asset_id, user_id, parameters, created_at = row
        
@@ -61,39 +55,55 @@ def migrateDataClaims(connection=None):
         
         print(f'coproductionprocess_id: {coproductionprocess_id}, asset_id: {asset_id}, user_id: {user_id}, assetId: {assetId}, task_id: {treeitem_id}, created_at {created_at}, commentTitle: {commentTitle}')
 
-        #Insert the new claim
+        # Insert the new claim
         query_insert_claim = """
             INSERT INTO claim (id, user_id, asset_id, task_id, coproductionprocess_id, title, description, state, claim_type)
             VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s) 
             ON CONFLICT (id) DO NOTHING RETURNING id;
         """
         values = (id,user_id, asset_id, treeitem_id, coproductionprocess_id, commentTitle, commentDescription, False, 'development')
-        cursor.execute(query_insert_claim, values)
         
-        result = cursor.fetchone()
-        
-        if result is not None:
-            claim_id = result[0]
-            connection.commit()  # Commit the transaction
+        try:
+            cursor.execute(query_insert_claim, values)
+            result = cursor.fetchone()
 
-            #Update the created_at field
-            query_update_claim = "UPDATE claim SET created_at = %s WHERE id = %s;"
-            values = (created_at, claim_id)
-            cursor.execute(query_update_claim, values)
+            if result is not None:
+                claim_id = result[0]
 
-            connection.commit()  # Commit the transaction
+                # Update the created_at field
+                query_update_claim = "UPDATE claim SET created_at = %s WHERE id = %s;"
+                values = (created_at, claim_id)
+                cursor.execute(query_update_claim, values)
+                connection.commit()  # Commit the transaction
+                
+                total_inserted_rows += 1
+                print(f'Claim inserted: claim_id: {claim_id} created_at: {created_at}')
+            else:
+                print(f'A row with id {id} already exists.')
 
-            total_inserted_rows += 1
+        except psycopg2.errors.ForeignKeyViolation as e:
+            # Handle the foreign key constraint violation here
+            print(f'Foreign key constraint violation: {e}')
+            
+            # Print the constraint name
+            constraint_name = e.diag.constraint_name
+            print(f'Constraint name: {constraint_name}')
+            
+            # Print the values that caused the violation
+            violated_values = e.diag.violation_column_values
+            print(f'Violated values: {violated_values}')
+            
+            # You can choose to log the error or take other actions as needed.
 
-            print(f'claim_id: {claim_id} created_at: {created_at}')
-
-        else:
-            print(f'A row with id {id} already exists.')
-    
     print(f'Total inserted rows: {total_inserted_rows}')
 
-        
+def main():
+    connection = create_connection()
+    if connection:
+        try:
+            migrate_data_claims(connection)
+        finally:
+            connection.close()
 
-       
-
-migrateDataClaims()    
+if __name__ == "__main__":
+    main()
